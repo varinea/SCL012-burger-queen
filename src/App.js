c1f23c6a3e965b98c1ec39ad3535c393b9cb4049
catalina-alzamora <56680334+catalina-alzamora@users.noreply.github.com>
2020-03-11 23:04:04 -0300

    cambiando vista de desayuno y agregando NewOrder (#14)
    * views!
    
    * firstview
    
    * newOrder.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import FirstView from './Components/FirstView.js';
-import {BrowserRouter as Router , Route , Switch} from 'react-router-dom';
-import  Menu  from './Components/Menu.js';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Menu from './Components/Menu.js';
 import OrdersB from './Components/OrdersB.js';
 import OrdersD from './Components/OrderD.js';
 
-function App (){
-  return(
+function App() {
+  return (
     <Router>
       <Switch>
-        <Route exact path ='/' component = { FirstView } />
-        <Route exact path ='/menu' component = { Menu } />
-        <Route exact path ='/OrdersB' component = { OrdersB } />
-        <Route exact path  ='/OrdersD' component = { OrdersD} />
+        <Route exact path='/' component={FirstView} />
+        <Route exact path='/menu' component={Menu} />
+        <Route exact path='/OrdersB' component={OrdersB} />
+        <Route exact path='/OrdersD' component={OrdersD} />
       </Switch>
     </Router>
   )
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,30 +1,30 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 import Header from '../Components/header';
-import {Button} from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 import '../css/Menu.css';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 class Menu extends Component {
-    render() {
-      return (
-        <div className='backgroundMenu'>
-          <div >
-            <Header titulo='Menu' />
-          </div>
-          
-          <div className='btnBreakfast'>
-            <Link to="/OrdersB">
-              <Button variant="warning" size="lg" block> Desayuno </Button>
-            </Link>
-            </div> 
-            <div className='btn-lunch'>
-              <Link to="/OrdersD">
-                <Button variant="warning" size="lg" block>Almuerzo o Cena</Button>
-              </Link>
-            </div>
+  render() {
+    return (
+      <div className='backgroundMenu'>
+        <div >
+          <Header titulo='Menu' />
         </div>
-      )
-  
-    }
+
+        <div className='btnBreakfast'>
+          <Link to="/OrdersB">
+            <Button variant="warning" size="lg" block> Desayuno </Button>
+          </Link>
+        </div>
+        <div className='btn-lunch'>
+          <Link to="/OrdersD">
+            <Button variant="warning" size="lg" block>Almuerzo o Cena</Button>
+          </Link>
+        </div>
+      </div>
+    )
+
+  }
 }
 export default Menu;
