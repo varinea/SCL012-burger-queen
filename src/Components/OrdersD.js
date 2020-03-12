039727a1895c34baa450c653967e1053c3dac482
varinea <varinea.lopez@gmail.com>
2020-03-12 12:47:34 -0300

    d

--- src/Components/OrdersD.js
+++ src/Components/OrdersD.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD:src/Components/OrdersD.js
-import React,{Component} from 'react';
-import Header from './header.js'
-import {Button} from 'react-bootstrap'
-import '../css/Menu.css';
-
-
-=======
 import React, { Component } from 'react';
 import Header from './header.js'
 import { Button } from 'react-bootstrap'
@@ -13,45 +5,44 @@ import '../css/Menu.css';
 
 
 
->>>>>>> 254522d9e974775aa5c6735e0146b6d107f5a2ae:src/Components/orderD.js
 class Dinner extends Component {
   render() {
     return (
       <div className='backgroundMenu'>
-        <div >
+        <div>
           <Header titulo='Almuerzo / Cena' />
         </div>
-        <div >
-          <div >
+        <div>
+          <div>
             <Header titulo='Hamburguesas' />
           </div>
           <Button variant="warning" size="lg" block> Hamburguesa Simple $1000 </Button>
         </div>
-        <div >
+        <div>
           <Button variant="warning" size="lg" block>Hamburguesa Doble $1500</Button>
         </div>
-        <div >
+        <div>
           <Header titulo='Acompañamientos' />
         </div>
-        <div >
+        <div>
           <Button variant="warning" size="lg" block>Papas fritas $500</Button>
         </div>
-        <div >
+        <div>
           <Button variant="warning" size="lg" block>Aros de cebolla $500</Button>
         </div>
-        <div >
+        <div>
           <Header titulo='Para Tomar' />
         </div>
-        <div >
+        <div>
           <Button variant="warning" size="lg" block>Agua 500ml $500</Button>
         </div>
-        <div >
+        <div>
           <Button variant="warning" size="lg" block>Agua 750ml $700</Button>
         </div>
-        <div >
+        <div>
           <Button variant="warning" size="lg" block>Agua 750ml $700</Button>
         </div>
-        <div >
+        <div>
           <Button variant="warning" size="lg" block>Bebida/gaseosa 500ml $700</Button>
         </div>
         <div>
